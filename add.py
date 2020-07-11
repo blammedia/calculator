2eecc4b6453fb2e7188d50f31cc61389cfa0d5e8
kingsj0405 <kingsj0405@gmail.com>
2020-07-11 17:41:22 +0900

    Implement add for inf

--- add.py
+++ add.py
@@ -1,10 +1,14 @@
-def add(a, b):
-    return a + b
+def add(input_list):
+    sum = 0
+    for i in input_list:
+        sum += i
+    return sum
 
 
 if __name__ == '__main__':
     print("[INFO] Start test for add")
-    assert(add(3, 5) == 8)
-    assert(add(1, -1) == 0)
-    assert(add(10000, 2000) == 12000)
+    assert(add([3, 5]) == 8)
+    assert(add([1, -1]) == 0)
+    assert(add([10000, 2000]) == 12000)
+    assert(add([1, 2, 3]) == 6)
     print("[INFO] Test success")
